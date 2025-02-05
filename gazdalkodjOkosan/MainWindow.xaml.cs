9ecd65e22548fc6c580c5959671db943f5d8c1c8
p4likas51 <schmitzhofer.pal@students.jedlik.eu>
2025-02-05 11:32:05 +0100

    #5

--- gazdalkodjOkosan/MainWindow.xaml.cs
+++ gazdalkodjOkosan/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System.IO;
+using System.Numerics;
 using System.Text;
 using System.Windows;
 using System.Windows.Controls;
@@ -17,14 +18,28 @@ namespace gazdalkodjOkosan
     /// </summary>
     public partial class MainWindow : Window
     {
+        Player player1 = new Player("p1", Brushes.Red);
         public MainWindow()
         {
             InitializeComponent();
 
             Loaded += (sender, e) =>
             {
-
+                GameGrid.Children.Add(player1.Shape);
+                UpdatePlayerPosition();
             };
+        }
+
+        private void UpdatePlayerPosition()
+        {
+            // Set the Grid.Row and Grid.Column properties for the player's position
+            Grid.SetRow(player1.Shape, player1.Row);
+            Grid.SetColumn(player1.Shape, player1.Column);
+        }
+        private void btnDice_Click(object sender, RoutedEventArgs e)
+        {
+            player1.MovePlayer();
+            UpdatePlayerPosition();
         }
     }
 }
